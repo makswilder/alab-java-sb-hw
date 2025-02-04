2a43b834a5a8a877f93c5dff4ff514a332c790bf
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2025-02-04 11:28:42 +0100

    SetUp Project On Spring Boot

---
+++ src/main/java/com/coworking/coworkingspace/CoworkingspaceApplication.java
@@ -0,0 +1,13 @@
+package com.coworking.coworkingspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coworking/coworkingspace/CoworkingspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworking.coworkingspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
