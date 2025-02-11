6cd5f32500ed06321d0a2e8d7b9dacbc48bbe785
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2025-02-11 23:46:26 +0100

    [HW14] Unit Tests AdminControllerTest fixed

--- src/test/java/com/coworking/coworkingspace/controller/AdminControllerTest.java
+++ src/test/java/com/coworking/coworkingspace/controller/AdminControllerTest.java
@@ -18,7 +18,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(AdminController.class)
 class AdminControllerTest {
 
     private MockMvc mockMvc;
